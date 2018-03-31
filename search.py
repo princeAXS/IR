ca09688226316b17deb03b49a48bc953e7d5a5df
uni5prince <phemraja@usc.edu>
2018-04-01 01:25:58 +1100

    added comments

--- search.py
+++ search.py
@@ -1,8 +1,7 @@
 import sys
-from normalise import normalise
 
 def getLexicon(lexiconFile):
-
+# Reads lexicons and the position of that lexicon in invlist file from memory
     lexiconPositionMap = {}
 
     with open(lexiconFile, 'r') as f:
@@ -13,7 +12,7 @@ def getLexicon(lexiconFile):
     return lexiconPositionMap
 
 def getDocNum(mapFile):
-
+# Reads docId and docNum from memory and make hash map of it
     docIDNumMap = []
 
     with open(mapFile, 'r') as f:
@@ -24,9 +23,10 @@ def getDocNum(mapFile):
     return docIDNumMap
 
 def getTermOccurance(term,lexiconFile = "lexicon",invertedListFile = "invlists",mapFile = "map"):
-
+# Reads the invlist and prints out term, its frequency, doc containing that term and its frequency within that document
     lexiconPositionMap = getLexicon(lexiconFile )
 
+    # if term not found then simply exits with showing a message
     if term not in lexiconPositionMap:
         print("Cannot find term:", term)
         return
@@ -37,14 +37,18 @@ def getTermOccurance(term,lexiconFile = "lexicon",invertedListFile = "invlists",
 
     offset = int(lexiconPositionMap[term])
 
+    #opens the file in binary mode to read bytes
     f = open(invertedListFile, 'rb')
 
+    #seeks to appropiate position of particular term in invlist file
     f.seek(offset, 0)
 
+    #reads first number at the offset that is list length or frequency of term occured in all documents
     listLength = int.from_bytes(f.read(4), byteorder='big')
 
     print(listLength)
 
+    #Loop through to get each docId in which term occured and its frequency
     while listLength>0:
         docID = int.from_bytes(f.read(4), byteorder='big')
         print(docIDNumMap[docID].rstrip(), end=" ")
@@ -57,6 +61,3 @@ if __name__ == '__main__':
 
     for term in termList:
         getTermOccurance(term,sys.argv[1],sys.argv[2],sys.argv[3])
-
-
-
