3b6632a6de9fba2c7531af43be42af37e15f7873
uni5prince <phemraja@usc.edu>
2018-05-04 02:14:13 +1000

    implemented argparser, running time, introduced stoplists, BM25 score for query as a whole, ranking by sorting the hash

--- search.py
+++ search.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 # pylint: disable=C0111,C0103,C0301
 
-import sys
+import sys,argparse,time
+from math import exp
 from normalise import normalise
+
+docScoreMap = {}
+AL,N,k1,b = 0,0,0,0
+
+def calculateK(Ld):
+    return k1*((1-b)+((b*Ld)/AL))
+
+def calculateBM25(ft, K, fdt):
+    return (exp((N-ft+0.5)/(ft+0.5)))*(((k1+1)*fdt)/(K+fdt))
+
+def getNAndAL(docMap):
+    sum = 0
+    for item in docMap:
+        sum += int(item[1])
+    return len(docMap),int(sum/len(docMap))
+
+def open_stoplist(sfn):
+    # Opens a stoplist stored on disk and returns it as a set of strings
+    if sfn is None:
+        # No stoplist to read - return a blank
+        return set()
+
+    with open(sfn, 'r') as sf:
+        # Read all words (strip them of whitespace) and return the set
+        # Set because order and identity don't matter, and there are good time gains to be had
+        sl = {w.strip() for w in sf}
+        return sl
 
 def getLexicon(lexiconFile):
     # Reads lexicons and the position of that lexicon in invlist file from memory
@@ -22,7 +50,7 @@ def getDocNum(mapFile):
     with open(mapFile, 'r') as f:
         for line in f:
             line = line.rstrip().split(" ")
-            docIDNumMap.append(line[1])
+            docIDNumMap.append([line[1],line[2]])
 
     return docIDNumMap
 
@@ -31,7 +59,7 @@ def getTermOccurance(term, invertedListFile, lexiconPositionMap, docIDNumMap):
     if term not in lexiconPositionMap:
         return
 
-    print(term)
+    # print(term)
 
     offset = int(lexiconPositionMap[term])
 
@@ -43,33 +71,80 @@ def getTermOccurance(term, invertedListFile, lexiconPositionMap, docIDNumMap):
 
     #reads first number at the offset that is list length or frequency of term occured in all documents
     listLength = int.from_bytes(f.read(4), byteorder='big')
+    ft = listLength
 
-    print(listLength)
+    # print(listLength)
 
     #Loop through to get each docId in which term occured and its frequency
     while listLength > 0:
-        docID = int.from_bytes(f.read(4), byteorder='big')
-        print(docIDNumMap[docID], end=" ")
-        print(str(int.from_bytes(f.read(4), byteorder='big')))
-        listLength -= 1
-    print("------------------")
+        docRow = docIDNumMap[int.from_bytes(f.read(4), byteorder='big')]
+        fdt = int.from_bytes(f.read(4), byteorder='big')
+        docId = docRow[0]
 
+        K = calculateK(int(docRow[1]))
+        # print(K,ft,fdt)
+        score = calculateBM25(ft, K, fdt)
+
+        if docId in docScoreMap:
+            docScoreMap[docId] += score
+        else:
+            docScoreMap[docId] = score
+
+        listLength -= 1
+    # print("------------------")
+    # print(docScoreMap)
+    # print("------------------")
+    # print("------------------")
 
 
 # Usage: ./search.py <lexicon> <invlists> <map> <queryterm 1> [... <queryterm N>]
 if __name__ == '__main__':
+    start_time = time.time()
     try:
-        termList = normalise(' '.join(sys.argv[4:]))
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-BM25', '--BM25', action='store_true',
+                            help='Uses BM25 similiarity function')
+        parser.add_argument('-q', '--querylabel', type=int,
+                            help='An integer that identiﬁes the current query')
+        parser.add_argument('-n', '--numresults', type=int,
+                            help='An integer number specifying the number of top-ranked documents that should be returned as an answer')
+        parser.add_argument('-l', '--lexicon', type=str,
+                            help='A path to a file containing a list of lexicons')
+        parser.add_argument('-i', '--invlists', type=str,
+                            help='A path to a Inverted list file')
+        parser.add_argument('-m', '--map', type=str,
+                            help='A path to a file containing a mapping table from internal document numbers to actual document identiﬁers')
+        parser.add_argument('-s', '--stoplist', type=str,
+                            help='A path to a file containing a list of stopwords')
+        parser.add_argument('queryterms', help='List of query terms', nargs='+')
+        args = parser.parse_args()
+
+        print(args.queryterms)
 
         # Reads docId and docNum from memory and make hash map of it
-        lex = getLexicon(sys.argv[1])
-        dmap = getDocNum(sys.argv[3])
+        lexicons = getLexicon(args.lexicon)
+        docMap = getDocNum(args.map)
+        stoplist = open_stoplist(args.stoplist)
 
-        if not termList:
-            print('No search query provided - exiting')
+        N,AL = getNAndAL(docMap)
+
+        k1 = 1.2
+        b = 0.75
+
+        termList = normalise(' '.join(args.queryterms), punctuation=r'[^a-z0-9\ ]+', case=False, stops=stoplist)
 
         for inputTerm in termList:
-            getTermOccurance(inputTerm, sys.argv[2], lex, dmap)
+            getTermOccurance(inputTerm, args.invlists, lexicons, docMap)
+
+        numOfResult = args.numresults
+        querylabel = args.querylabel
+
+        i = 0
+        for r in sorted(docScoreMap, key=docScoreMap.get, reverse=True):
+            print(querylabel, r, i, docScoreMap[r])
+            i += 1
+
+        print("Running time: %d ms" % ((time.time() - start_time) * 1000))
     except OSError as e:
         print('{}\nProgram Exiting'.format(e))
     except IndexError:
