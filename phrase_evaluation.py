d0f05fda07dfdc4c11a68e69233d1c6c7926cff9
CSymes <CaryJSymes@gmail.com>
2018-05-21 04:10:24 +1000

    Un-hardcode pathing for evaluation

--- phrase_evaluation.py
+++ phrase_evaluation.py
@@ -10,29 +10,50 @@
 ######################################
 
 import subprocess
+import sys
 import os
 
 if __name__ == '__main__':
+    # Input file data
     Q_TOPICS = {}
     Q_REL = {}
 
-    SRC_ROOT = './'
-    ROOT = 'analytics'
+    # Directory for output files
+    OUT_ROOT = 'analytics'
+    # Save output files?
+    STALE = True
 
+    # Default input paths
+    SRC_ROOT = '.'
+    TOP_FN = 'topics'
+    REL_FN = 'qrels'
+
+    # Results store
     RESULTS_BM25   = {}
     RESULTS_PHRASE = {}
 
+    # Num results and precision measure
     BM25_SIZE = 20
     PRECISION = 10
 
-    STALE = True
 
-    with open(SRC_ROOT + 'topics', 'r') as ft:
+    # Allow args for input paths
+    ARG_L = len(sys.argv)
+    if ARG_L >= 5 or ARG_L == 3:
+        sys.exit('Incorrect number of arguments - Usage: phrase_evauluation.py [path to input files] [<topics> <query relevances>]')
+    if ARG_L == 4:
+        TOP_FN, REL_FN = sys.argv[2:4]
+    if ARG_L >= 2:
+        SRC_ROOT = sys.argv[1]
+
+    with open(SRC_ROOT + '/' + TOP_FN, 'r') as ft:
         for topic in ft:
             topic = topic.split()
             Q_TOPICS[topic[0]] = topic[1:]
 
-    with open(SRC_ROOT + 'qrels', 'r') as fr:
+
+
+    with open(SRC_ROOT + '/' + REL_FN, 'r') as fr:
         for rel in fr:
             rel = rel.split()
 
@@ -41,8 +62,8 @@ if __name__ == '__main__':
 
             Q_REL[rel[0]][rel[2].lower()] = rel[3] == '1' # Store boolean
 
-    if not os.path.exists(ROOT):
-        os.makedirs(ROOT)
+    if not os.path.exists(OUT_ROOT):
+        os.makedirs(OUT_ROOT)
 
     for qn, t in Q_TOPICS.items():
         print(qn, ' '.join(t))
@@ -105,12 +126,12 @@ if __name__ == '__main__':
 
         if STALE:
             # Save results to disk
-            with open('{}/{}_phrase'.format(ROOT, qn), 'w') as ph_out:
+            with open('{}/{}_phrase'.format(OUT_ROOT, qn), 'w') as ph_out:
                 for e in RESULTS_PHRASE[qn]:
                     REL = Q_REL[qn]
                     ph_out.write('{} {} {}\n'.format(*(e + (1 if (e[0] in REL and REL[e[0]]) else 0,))))
 
-            with open('{}/{}_bm25'.format(ROOT, qn), 'w') as bm_out:
+            with open('{}/{}_bm25'.format(OUT_ROOT, qn), 'w') as bm_out:
                 for e in RESULTS_BM25[qn]:
                     REL = Q_REL[qn]
                     bm_out.write('{} {} {}\n'.format(*(e + (1 if (e[0] in REL and REL[e[0]]) else 0,))))
